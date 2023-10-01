11628e2f00f17d33c0a2e048b235efb470ee9ac4
Hossein <h.dvd13@gmail.com>
2023-10-01 15:49:55 -0700

    feat: add db file to gitignore

---
+++ server/migrations/versions/216a1746d87e_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 216a1746d87e
+Revises: 
+Create Date: 2023-10-01 15:43:35.142386
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '216a1746d87e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
